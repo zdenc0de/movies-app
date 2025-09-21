69cdf2defda2def7cf83d6ec4ce6e2e67924f74e
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-21 11:42:12 -0600

    project files added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const App1 = () => {
+  return (
+    <main>
+      <div className="pattern" /> 
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">
+              Movies
+            </span>
+            You'll Enjoy Without The Hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  )
+}
+export default App1
