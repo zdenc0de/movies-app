815969ab7e88cdae3702ea2f370d6ef2b84d792f
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-09-22 10:44:44 -0600

    Movies shown in the console

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Search } from "./components/Search"
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("")
-  return (
+
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies")
+      }
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(`Something went wrong: ${error.message}`)
+      setErrorMessage(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+    return (
     <main>
       <div className="pattern" /> 
       <div className="wrapper">
@@ -15,9 +47,13 @@ const App = () => {
             </span>
             You'll Enjoy Without The Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white text-3xl">{searchTerm}</h1>
+
+        <section className="all-movies">
+          <h2>All movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   )
